6421c492ba3a1ec3e6b7c0ae15f247c18ea40f08
bhagya314 <bhagyashrilalsare@gmail.com>
2021-03-26 19:08:20 +0530

    multiplication

---
+++ C_Programs/Three_no_mul.C
@@ -0,0 +1,14 @@
+void main()
+{
+int a,b,c;
+int result;
+printf("enter the value of a");
+scanf("%d",&a);
+printf("enter the value of b");
+scanf("%d",&b);
+printf("enter the valur of c");
+scanf("%d",&c);
+result=a*b*c;
+printf("%d",result);
+getch();
+}
