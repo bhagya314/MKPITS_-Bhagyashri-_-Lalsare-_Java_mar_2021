4be3da0165b990fcd0c221c57d7fc2ae7de03751
bhagya314 <bhagyashrilalsare@gmail.com>
2021-04-10 12:01:44 +0530

    while loop

---
+++ C_Programs/326_Print1To10UsingWhileLoop.cpp
@@ -0,0 +1,12 @@
+//wap to print no from 1 to 10 using while loop
+#include<stdio.h>
+int main()
+{
+	int cnt=1;
+	while(cnt<=10)
+	{
+		printf("%d\t",cnt);
+		cnt = cnt +1;
+	}
+	return 0;
+}
---
+++ C_Programs/326_Print50To40ByWhileLoop.cpp
@@ -0,0 +1,12 @@
+//wap to print no. from 50 to 40 using while loop.
+#include<stdio.h>
+int main()
+{
+	int cnt=50;
+	while(cnt>40)
+	{
+		printf("%d\t",cnt);
+		cnt = cnt -1;
+	}
+	return 0;
+}
