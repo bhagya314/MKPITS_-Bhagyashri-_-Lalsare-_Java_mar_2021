dddf4ec5deba0154b360678b3c5574bffa35491c
bhagya314 <bhagyashrilalsare@gmail.com>
2021-04-13 20:03:16 +0530

    7th april Data type

---
+++ Basic_Java_Programs/DoubleDatatype.java
@@ -0,0 +1,17 @@
+//Double Data Type
+
+
+import java.util.*;
+public class DoubleDatatype
+{
+    public static void main(String args[])
+    {
+        double b = 1234.567890123456789012345678;
+        System.out.println("b ="+b);
+        System.out.println("enter number");
+
+        Scanner s=new Scanner(System.in);
+        b=s.nextDouble();
+        System.out.println(" number = " + b);
+    }
+}
---
+++ Basic_Java_Programs/FloatDatatype.java
@@ -0,0 +1,15 @@
+//Float data type
+
+import java.util.*;
+public class FloatDatatype
+{
+    public static void main(String args[])
+    {
+        float b = 1234.56789011f;
+        System.out.println("b ="+b);
+        System.out.println("enter number");
+        Scanner s=new Scanner(System.in);
+        b=s.nextFloat();
+        System.out.println(" number = " + b);
+    }
+}
