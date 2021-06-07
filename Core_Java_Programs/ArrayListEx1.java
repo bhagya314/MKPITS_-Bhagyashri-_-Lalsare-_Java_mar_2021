ab42961e959cd93252a5860abfd47e0466b9bdf5
bhagya314 <bhagyashrilalsare@gmail.com>
2021-06-07 10:38:57 +0530

    24th may

---
+++ Core_Java_Programs/ArrayListEx1.java
@@ -0,0 +1,16 @@
+//Java ArrayList Example
+package Com.Mkpits.Java.AWT.May24;
+import java.util.*;
+public class ArrayListEx1 {
+    public static void main(String args[]){
+        ArrayList<String> list=new ArrayList<String>();//Creating arraylist
+        list.add("Mango");
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Grapes");
+
+        //Printing the arraylist object
+        System.out.println(list);
+    }
+}
+
